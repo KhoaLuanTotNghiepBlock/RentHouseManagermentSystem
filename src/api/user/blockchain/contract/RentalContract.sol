e7ebb32ba764ba9dbb86f03391ccc4c0a4510c76
Giang Vo <tragiang@bacoor.co>
2023-03-21 13:36:00 +0700

    update contract

--- src/api/user/blockchain/contract/RentalContract.sol
+++ src/api/user/blockchain/contract/RentalContract.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 contract RentalContract {
     string public contractTransactionId;
+    string public invoiceId;
     bool public signedByOwner;
     bool public signedByRenter;
     uint public rentAmount;
@@ -10,8 +11,9 @@ contract RentalContract {
     address payable public owner;
     address payable public renter;
 
-    event RentStarted(address owner, uint rentAmount, uint depositAmount);
-    event RentEnded(address renter, uint depositReturned);
+    event RentStarted(address renter, uint rentAmount, uint depositAmount);
+    event RentMonth(address renter, uint rentAmount);
+    event RentEnded(address owner, uint depositReturned);
 
     constructor(
         string memory _contractTransactionId,
@@ -27,17 +29,25 @@ contract RentalContract {
         depositAmount = _depositAmount;
     }
 
-    function signByOwner() public {
-        signedByOwner = true;
-    }
-
+    // only renter cna sign
     function signByRenter() public payable {
         signedByRenter = true;
+        require(msg.value >= rentAmount + depositAmount, "!balance");
         emit RentStarted(renter, rentAmount, depositAmount);
+        owner.transfer(rentAmount);
     }
 
+    // TODO: payment by month
+    function payForRentByMonth(string memory _invoiceId) public payable {
+        require(msg.value >= rentAmount, "!balance");
+        emit RentMonth(renter, rentAmount);
+        owner.transfer(rentAmount);
+        invoiceId = _invoiceId;
+    }
+
+    // TODO: only owner can call
     function endRent(string memory _contractTransactionId) public {
-        owner.transfer(rentAmount + depositAmount);
+        renter.transfer(depositAmount);
         contractTransactionId = _contractTransactionId;
         emit RentEnded(msg.sender, depositAmount);
     }
