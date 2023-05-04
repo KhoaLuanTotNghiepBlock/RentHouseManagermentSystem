16206e389db9e5eefe34a894f821f39e6f83fbb4
Giang Vo <tragiang@bacoor.co>
2023-05-04 09:45:13 +0700

    update

--- src/config/cron.js
+++ src/config/cron.js
@@ -3,7 +3,7 @@ const CronService = require("../api/user/service/cron.service");
 const RequestService = require("../api/user/service/request.service");
 const demandTime = "*/360 * * * *";
 const invoiceTime = "*/360 * * * *";
-const deleteTime = "*/10 * * * *";
+const deleteTime = "*/3 * * * *";
 // // 8AM on thr 15th day every month
 // cron.schedule('0 8 15 * *', () => {
 //     console.log('This cron job runs on the 15th day of every month at 8:00 AM');
@@ -11,23 +11,23 @@ const deleteTime = "*/10 * * * *";
 
 cron.schedule("*/180 * * * *", async () => {
     console.log("AUTO REQUEST ===> START");
-    // await RequestService.executeRequestInDue();
+    await RequestService.executeRequestInDue();
     console.log("AUTO REQUEST ===> END");
 });
 
 cron.schedule(demandTime, async () => {
     console.log("AUTO SERVICE DEMANDS ===> START");
-    // await CronService.autoCreateInvoice();
+    await CronService.autoCreateInvoice();
     console.log("AUTO SERVICE DEMANDS ===> END");
 });
 
 cron.schedule(invoiceTime, async () => {
     console.log("AUTO INVOICE PAYMENT ===> START");
-    // await CronService.autoPayForInvoice();
+    await CronService.autoPayForInvoice();
     console.log("AUTO INVOICE PAYMENT ===> END");
 });
 
-cron.schedule(invoiceTime, async () => {
+cron.schedule(deleteTime, async () => {
     console.log("AUTO DELETE ROOM FAIL===> START");
     await CronService.deleteRoomFail();
     console.log("AUTO DELETE ROOM FAIL ===> END");
